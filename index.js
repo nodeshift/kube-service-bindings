332b880d84808bb8cbb3269363148992617d033a
Michael Dawson <mdawson@devrus.com>
2021-04-20 16:24:44 -0400

    add linting

--- index.js
+++ index.js
@@ -2,40 +2,45 @@
 const fs = require('fs');
 const path = require('path');
 
-const  services = new Object();
+const services = {};
 
 // services and clients that kube-service-bindings understands
-services['KAFKA'] =
-  { 'type': 'kafka',
-    'clients': { 
-      'node-rdkafka': { 'mapping': { 'securityProtocol': 'security.protocol',
-                                     'bootstrapServers': 'bootstrap.servers',
-                                     'clientId': 'client.id',
-                                     'saslMechanism': 'sasl.mechanisms',
-                                     'type': '',
-                                     'provider': '',
-                                     'clientSecret': '',
-                                     'user': 'sasl.username',
-                                     'password': 'sasl.password'
-                                   },
-	              },
-      'kafkajs'     : { 'mapping': { 'securityProtocol': 'ssl',
-                                     'bootstrapServers': ['brokers'],
-                                     'clientId': 'clientId',
-                                     'saslMechanism': {'sasl': 'mechanism'},
-                                     'type': '',
-                                     'provider': '',
-                                     'clientSecret': '',
-                                     'user': { 'sasl': 'username' },
-                                     'password': { 'sasl': 'password' }
-                                   },
-	                'valueMapping': { 'ssl': { 'SASL_SSL': true } },
-	                'transform': (binding) => {
-			  if (binding.sasl && binding.sasl.mechanism) {
-		            binding.sasl.mechanism = binding.sasl.mechanism.toLowerCase();
-	                  }
-			}
-	              }
+services.KAFKA =
+  {
+    type: 'kafka',
+    clients: {
+      'node-rdkafka': {
+        mapping: {
+          securityProtocol: 'security.protocol',
+          bootstrapServers: 'bootstrap.servers',
+          clientId: 'client.id',
+          saslMechanism: 'sasl.mechanisms',
+          type: '',
+          provider: '',
+          clientSecret: '',
+          user: 'sasl.username',
+          password: 'sasl.password'
+        }
+      },
+      kafkajs: {
+        mapping: {
+          securityProtocol: 'ssl',
+          bootstrapServers: ['brokers'],
+          clientId: 'clientId',
+          saslMechanism: { sasl: 'mechanism' },
+          type: '',
+          provider: '',
+          clientSecret: '',
+          user: { sasl: 'username' },
+          password: { sasl: 'password' }
+        },
+        valueMapping: { ssl: { SASL_SSL: true } },
+        transform: (binding) => {
+          if (binding.sasl && binding.sasl.mechanism) {
+            binding.sasl.mechanism = binding.sasl.mechanism.toLowerCase();
+          }
+        }
+      }
     }
   };
 
@@ -43,17 +48,17 @@ services['KAFKA'] =
 // either set the value directly on the binding object
 // passed in or create a sub-object on the binding and
 // then call setKey recursively to set the value
-function setKey(binding, key, value) {
+function setKey (binding, key, value) {
   if (key) {
-    if (typeof key === "string") {
-      binding[key] =  value;
-    } else if (typeof key === "object") {
+    if (typeof key === 'string') {
+      binding[key] = value;
+    } else if (typeof key === 'object') {
       if (Array.isArray(key)) {
         binding[key[0]] = new Array(value);
       } else {
-        for (let subkey in key) {
+        for (const subkey in key) {
           if (!binding[subkey]) {
-            binding[subkey] = new Object();
+            binding[subkey] = {};
           }
           setKey(binding[subkey], key[subkey], value);
         }
@@ -63,7 +68,7 @@ function setKey(binding, key, value) {
 }
 
 // return the bidings requested
-function getBinding(type, client, id) {
+function getBinding (type, client, id) {
   // validate we know about the type
   if (!services[type]) {
     throw new Error('Unknown service type');
@@ -71,37 +76,36 @@ function getBinding(type, client, id) {
 
   // validate that we know about the requested client
   if (client && (!services[type].clients ||
-	         !services[type].clients[client])) {
+                 !services[type].clients[client])) {
     throw new Error('Unknown client');
   }
 
   // find the matching binding
   let bindingsRoot = null;
-  const root = process.env['SERVICE_BINDING_ROOT'];
+  const root = process.env.SERVICE_BINDING_ROOT;
   if (root) {
     const candidates = fs.readdirSync(root);
-    for (let file of candidates) {
+    for (const file of candidates) {
       try {
         const bindingType =
           fs.readFileSync(path.join(root, file, 'type')).toString().trim();
         if (bindingType === services[type].type) {
           if ((id === undefined) || file.includes(id)) {
-            bindingsRoot = path.join(root,file);
+            bindingsRoot = path.join(root, file);
             break;
           }
-        };
-      } catch (err) { };
-    };
-  };
+        }
+      } catch (err) { }
+    }
+  }
 
   // bail if we did not find a binding
   if (bindingsRoot == null) {
     throw new Error('No Binding Found');
   }
 
-
   // read and convert the available binding info
-  let binding = new Object();
+  const binding = {};
   const bindingFiles = fs.readdirSync(bindingsRoot);
   bindingFiles.forEach((file) => {
     if (!file.startsWith('..')) {
@@ -110,7 +114,7 @@ function getBinding(type, client, id) {
           fs.readFileSync(path.join(bindingsRoot, file)).toString();
 
       if (client) {
-	const clientInfo = services[type].clients[client];
+        const clientInfo = services[type].clients[client];
         if (client && (clientInfo.mapping[key] ||
                        clientInfo.mapping[key] === '')) {
           key = clientInfo.mapping[key];
@@ -120,13 +124,13 @@ function getBinding(type, client, id) {
         if ((clientInfo.valueMapping) &&
             (clientInfo.valueMapping[key])) {
           value = clientInfo.valueMapping[key][value];
-	}
+        }
 
         // set the key
         setKey(binding, key, value);
 
         // do any final transforms needed
-        if(clientInfo.transform) {
+        if (clientInfo.transform) {
           clientInfo.transform(binding);
         }
       } else {
@@ -135,7 +139,7 @@ function getBinding(type, client, id) {
     }
   });
 
- return binding;
+  return binding;
 }
 
 module.exports.getBinding = getBinding;
