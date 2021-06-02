c3f27d72ed7edb5d4f21bc5ee3fc780550c6e3af
Evan Shortiss <evanshortiss@gmail.com>
2021-06-02 19:09:35 +0100

    fix: typings do not need module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
 import { KafkaConfig } from 'kafkajs'
 
-declare module 'kube-service-bindings' {
-  export function getBinding(service: 'KAFKA', client: 'node-rdkafka'): unknown;
-  export function getBinding(service: 'KAFKA', client: 'kafkajs'): KafkaConfig;
-}
+export function getBinding(service: 'KAFKA', client: 'node-rdkafka'): unknown;
+export function getBinding(service: 'KAFKA', client: 'kafkajs'): KafkaConfig;
