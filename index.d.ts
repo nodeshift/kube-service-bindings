d60bc066d7b1119d84edf1be48e8d00bfdb1f820
Evan Shortiss <evanshortiss@gmail.com>
2021-06-02 19:30:28 +0100

    feat: add node-rdkafka return type in typings

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { KafkaConfig } from 'kafkajs'
+import { GlobalConfig } from 'node-rdkafka';
 
-export function getBinding(service: 'KAFKA', client: 'node-rdkafka'): unknown;
+export function getBinding(service: 'KAFKA', client: 'node-rdkafka'): GlobalConfig;
 export function getBinding(service: 'KAFKA', client: 'kafkajs'): KafkaConfig;
