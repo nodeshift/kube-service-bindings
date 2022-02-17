2b183d4a069d8d9f8aa8bed093e683facf07b702
Michael Dawson <mdawson@devrus.com>
2022-02-17 16:44:44 -0500

    feat: improve mongo support (#25)
    * feat: improve mongo support
    
    - build url needed to connect
    - move auth info to the right nesting
    - add test for case when port is used
    
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- clients/MONGODB/mongodb.js
+++ clients/MONGODB/mongodb.js
@@ -1,10 +1,35 @@
 // Binding for mongodb client
 module.exports = {
   mapping: {
-    username: 'user',
+    user: { connectionOptions: { auth: 'username' } },
+    password: { connectionOptions: { auth: 'password' } },
     host: 'host',
-    password: 'password',
     port: 'port',
-    srv: 'srv'
+    srv: 'srv',
+    type: ''
+  },
+  transform: (binding) => {
+    let userPassword = '';
+    if (binding.connectionOptions &&
+        binding.connectionOptions.auth &&
+        binding.connectionOptions.auth.username) {
+      const encodedUser =
+        encodeURIComponent(binding.connectionOptions.auth.username);
+      let encodedPassword = '';
+      if (binding.connectionOptions.auth.password) {
+        encodedPassword =
+          encodeURIComponent(binding.connectionOptions.auth.password);
+      }
+      userPassword = `${encodedUser}:${encodedPassword}@`;
+    }
+    if (binding.srv === 'true') {
+      binding.url = `mongodb+srv://${userPassword}${binding.host}`;
+    } else {
+      let port = '';
+      if (binding.port) {
+        port = `:${binding.port}`;
+      }
+      binding.url = `mongodb://${userPassword}${binding.host}${port}`;
+    }
   }
 };
