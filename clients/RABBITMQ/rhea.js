ff9d708a53724f8754d2db4bb171c3d6286986a3
Costas Papastathis <papastathiscr@gmail.com>
2022-02-22 13:18:38 +0200

    feat: adding mapping for RABBITMQ with rhea client

---
+++ clients/RABBITMQ/rhea.js
@@ -0,0 +1,10 @@
+// Binding for rheajs client
+module.exports = {
+  mapping: {
+    host: 'host',
+    password: 'password',
+    node: 'node',
+    port: 'port',
+    username: 'username'
+  }
+};
