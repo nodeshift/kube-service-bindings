552461ca66db663418fb9e934296254f322d13d2
Costas Papastathis <papastathiscr@gmail.com>
2022-04-28 17:05:01 +0300

    fix: transform function in order to return a string for the connection

--- clients/MYSQL/odbc.js
+++ clients/MYSQL/odbc.js
@@ -6,5 +6,25 @@ module.exports = {
     database: 'database',
     password: 'PWD',
     type: 'DSN'
+  },
+  transform: (binding) => {
+    console.log('got called');
+    if (
+      binding.host &&
+      binding.port &&
+      binding.UID &&
+      binding.database &&
+      binding.PWD
+    ) {
+      console.log('im in');
+      binding.connectionString = [
+        `Driver=MySQL`,
+        `Uid=${binding.UID}`,
+        `Pwd=${binding.PWD}`,
+        `Database=${binding.database}`,
+        `Server=${binding.host}`,
+        `Port:${binding.port}`
+      ].join(';');
+    }
   }
 };
