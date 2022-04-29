46b063f3023ada52fed1811e00f7ab8bb44cc824
Costas Papastathis <papastathiscr@gmail.com>
2022-04-29 13:19:42 +0300

    feat: adding transform function to get the connectionString

--- clients/POSTGRESQL/odbc.js
+++ clients/POSTGRESQL/odbc.js
@@ -8,5 +8,23 @@ module.exports = {
     'ca.crt': { ssl: 'ca' },
     'tls.key': { ssl: 'key' },
     'tls.crt': { ssl: 'cert' }
+  },
+  transform: (binding) => {
+    if (
+      binding.user &&
+      binding.host &&
+      binding.database &&
+      binding.password &&
+      binding.port
+    ) {
+      binding.connectionString = [
+        `DRIVER=PostgreSQL`,
+        `USER=${binding.user}`,
+        `SERVER=${binding.host}`,
+        `DATABASE=${binding.database}`,
+        `PASSWORD=${binding.password}`,
+        `PORT:${binding.port}`
+      ].join(';');
+    }
   }
 };
