95be676baa0e9652f3c54e6d9dee7a530bdb5647
Costas Papastathis <papastathiscr@gmail.com>
2022-04-29 17:47:09 +0300

    adding all the mysql credential files in tests that percona provides

--- test/test-mysql-binding/test.js
+++ test/test-mysql-binding/test.js
@@ -16,22 +16,27 @@ describe('On MySQL Database', () => {
     });
     assert(binding);
     assert.deepEqual(binding, {
-      clusterIP: 'None',
+      'auto-config.cnf':
+        '[mysqld]\ninnodb_buffer_pool_size = 750000000\nmax_connections = 79',
+      'ca.crt': `-----BEGIN CERTIFICATE-----\nFGH\n-----END CERTIFICATE-----`,
       clustercheck: 'clustercheckpassword',
+      clusterIP: 'None',
+      database: 'test',
+      host: 'localhost',
       monitor: 'monitory',
       operator: 'operatoradmin',
+      password: 'password',
       pmmserver: 'admin',
+      port: 3306,
       provider: 'percona',
       proxyadmin: 'admin_password',
       replication: 'repl_password',
       root: 'root_password',
-      xtrabackup: 'backup_password',
-      host: 'localhost',
-      port: 3306,
-      database: 'test',
+      'tls.crt': `-----BEGIN CERTIFICATE-----\nABC\n-----END CERTIFICATE-----`,
+      'tls.key': `-----BEGIN RSA PRIVATE KEY-----\nCDE\n-----END RSA PRIVATE KEY-----`,
+      type: 'mysql',
       user: 'root',
-      password: 'password',
-      type: 'mysql'
+      xtrabackup: 'backup_password'
     });
   });
 
