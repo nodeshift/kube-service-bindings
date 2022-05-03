4334a09bb3ddfb44f24a0f61ef81a98d11726468
Costas Papastathis <papastathiscr@gmail.com>
2022-05-03 17:44:25 +0300

    feat: adding mysql client support with tests and all

--- test/test-mysql-binding/test.js
+++ test/test-mysql-binding/test.js
@@ -49,6 +49,45 @@ describe('On MySQL Database', () => {
     });
   });
 
+  it('fetches Unmapped credentials for mysql client', () => {
+    const binding = bindings.getBinding('MYSQL', 'mysql', {
+      removeUnmapped: false
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      clusterIP: 'None',
+      clustercheck: 'clustercheckpassword',
+      monitor: 'monitory',
+      operator: 'operatoradmin',
+      pmmserver: 'admin',
+      provider: 'percona',
+      proxyadmin: 'admin_password',
+      replication: 'repl_password',
+      root: 'root_password',
+      xtrabackup: 'backup_password',
+      host: 'localhost',
+      port: 3306,
+      database: 'test',
+      user: 'root',
+      password: 'password',
+      type: 'mysql'
+    });
+  });
+
+  it('fetches credentials for mysql client filtered by mappings', () => {
+    const binding = bindings.getBinding('MYSQL', 'mysql', {
+      removeUnmapped: true
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      host: 'localhost',
+      port: 3306,
+      database: 'test',
+      user: 'root',
+      password: 'password'
+    });
+  });
+
   after(() => {
     process.env = env;
   });
