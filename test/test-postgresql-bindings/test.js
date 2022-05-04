96fa574e0443bd220ea43f211163c9f174277947
Costas Papastathis <papastathiscr@gmail.com>
2022-05-04 12:46:20 +0300

    fix: addings tests for odbc psql client

--- test/test-postgresql-bindings/test.js
+++ test/test-postgresql-bindings/test.js
@@ -28,6 +28,39 @@ describe('test-postgresql-bindings', () => {
     });
   });
 
+  it('fetches Unmapped credentials for odbc client in PSQL', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'odbc', {
+      removeUnmapped: false
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      database: 'db1',
+      host: '127.0.0.1',
+      password: 'p1',
+      port: '1234',
+      type: 'postgresql',
+      user: 'michael',
+      connectionString:
+        'DRIVER=PostgreSQL;SERVER=127.0.0.1;DATABASE=db1;PORT:1234;USER=michael;PASSWORD=p1',
+      ssl: {
+        ca: '----BEGIN CERTIFICATE-----\n1234\n-----END CERTIFICATE-----',
+        cert: '-----BEGIN CERTIFICATE-----\nABCD\n-----END CERTIFICATE-----',
+        key: '-----BEGIN CERTIFICATE-----\nXYZ\n-----END CERTIFICATE-----'
+      }
+    });
+  });
+
+  it('fetches credentials for odbc client in PSQL filtered by mappings', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'odbc', {
+      removeUnmapped: true
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      connectionString:
+        'DRIVER=PostgreSQL;SERVER=127.0.0.1;DATABASE=db1;PORT:1234;USER=michael;PASSWORD=p1'
+    });
+  });
+
   after(() => {
     process.env = env;
   });
