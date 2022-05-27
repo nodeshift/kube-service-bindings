8087d86fb2d8fc1124b8ea66a643575a9aea311f
Costas Papastathis <papastathiscr@gmail.com>
2022-05-27 19:50:35 +0300

    test: adding tests for testing valueFilepath and in general increase the number of tests

--- test/test-postgresql-bindings/test.js
+++ test/test-postgresql-bindings/test.js
@@ -3,6 +3,8 @@ const path = require('path');
 const { after, before, describe, it } = require('mocha');
 const bindings = require('../../index.js');
 
+const { bindedFiles, connectionString } = require('./assertionObjects');
+
 describe('test-postgresql-bindings', () => {
   let env;
   before(() => {
@@ -10,8 +12,52 @@ describe('test-postgresql-bindings', () => {
     process.env = { SERVICE_BINDING_ROOT: path.join(__dirname, 'bindings') };
   });
 
-  it('test-postgres-pg', () => {
-    const binding = bindings.getBinding('POSTGRESQL', 'pg');
+  it('Testing pg client with id and removeUnmapped option', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'pg', {
+      id: 'postgresql-bindings',
+      removeUnmapped: true
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      database: 'db1',
+      host: '127.0.0.1',
+      password: 'p1',
+      port: '1234',
+      type: 'postgresql',
+      user: 'michael',
+      ssl: {
+        ca: '----BEGIN CERTIFICATE-----\n1234\n-----END CERTIFICATE-----',
+        cert: '-----BEGIN CERTIFICATE-----\nABCD\n-----END CERTIFICATE-----',
+        key: '-----BEGIN CERTIFICATE-----\nXYZ\n-----END CERTIFICATE-----'
+      }
+    });
+  });
+
+  it('Testing pg client with id and removeUnmapped option to false', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'pg', {
+      id: 'postgresql-bindings',
+      removeUnmapped: false
+    });
+    assert(binding);
+    assert.deepEqual(binding, {
+      database: 'db1',
+      host: '127.0.0.1',
+      password: 'p1',
+      port: '1234',
+      type: 'postgresql',
+      user: 'michael',
+      ssl: {
+        ca: '----BEGIN CERTIFICATE-----\n1234\n-----END CERTIFICATE-----',
+        cert: '-----BEGIN CERTIFICATE-----\nABCD\n-----END CERTIFICATE-----',
+        key: '-----BEGIN CERTIFICATE-----\nXYZ\n-----END CERTIFICATE-----'
+      }
+    });
+  });
+
+  it('Testing pg client with id and mapped option default behaviour', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'pg', {
+      id: 'postgresql-bindings'
+    });
     assert(binding);
     assert.deepEqual(binding, {
       database: 'db1',
@@ -32,33 +78,76 @@ describe('test-postgresql-bindings', () => {
     const binding = bindings.getBinding('POSTGRESQL', 'odbc', {
       removeUnmapped: false
     });
+
+    const sslrootcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/ca.crt`;
+    const sslcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.crt`;
+    const sslkey = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.key`;
+
+    const pqopt = [
+      `{`,
+      `sslrootcert=${sslrootcert}`,
+      `sslcert=${sslcert}`,
+      `sslkey=${sslkey}`,
+      `}`
+    ].join(' ');
+
+    const validationObject = {
+      ...bindedFiles,
+      sslrootcert,
+      sslcert,
+      sslkey,
+      connectionString: `pqopt=${pqopt};${connectionString}`
+    };
     assert(binding);
-    assert.deepEqual(binding, {
-      database: 'db1',
-      host: '127.0.0.1',
-      password: 'p1',
-      port: '1234',
-      type: 'postgresql',
-      user: 'michael',
-      connectionString:
-        'DRIVER=PostgreSQL;SERVER=127.0.0.1;DATABASE=db1;PORT:1234;USER=michael;PASSWORD=p1',
-      ssl: {
-        ca: '----BEGIN CERTIFICATE-----\n1234\n-----END CERTIFICATE-----',
-        cert: '-----BEGIN CERTIFICATE-----\nABCD\n-----END CERTIFICATE-----',
-        key: '-----BEGIN CERTIFICATE-----\nXYZ\n-----END CERTIFICATE-----'
-      }
-    });
+    assert.deepEqual(binding, validationObject);
   });
 
   it('fetches credentials for odbc client in PSQL filtered by mappings', () => {
     const binding = bindings.getBinding('POSTGRESQL', 'odbc', {
       removeUnmapped: true
     });
+
+    const sslrootcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/ca.crt`;
+    const sslcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.crt`;
+    const sslkey = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.key`;
+
+    const pqopt = [
+      `{`,
+      `sslrootcert=${sslrootcert}`,
+      `sslcert=${sslcert}`,
+      `sslkey=${sslkey}`,
+      `}`
+    ].join(' ');
+
+    const validationObject = {
+      connectionString: `pqopt=${pqopt};${connectionString}`
+    };
+
     assert(binding);
-    assert.deepEqual(binding, {
-      connectionString:
-        'DRIVER=PostgreSQL;SERVER=127.0.0.1;DATABASE=db1;PORT:1234;USER=michael;PASSWORD=p1'
-    });
+    assert.deepEqual(binding, validationObject);
+  });
+
+  it('fetches credentials for odbc client in PSQL default behaviour', () => {
+    const binding = bindings.getBinding('POSTGRESQL', 'odbc');
+
+    const sslrootcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/ca.crt`;
+    const sslcert = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.crt`;
+    const sslkey = `${process.env.SERVICE_BINDING_ROOT}/crunchy-data-postgres-operator/tls.key`;
+
+    const pqopt = [
+      `{`,
+      `sslrootcert=${sslrootcert}`,
+      `sslcert=${sslcert}`,
+      `sslkey=${sslkey}`,
+      `}`
+    ].join(' ');
+
+    const validationObject = {
+      connectionString: `pqopt=${pqopt};${connectionString}`
+    };
+
+    assert(binding);
+    assert.deepEqual(binding, validationObject);
   });
 
   after(() => {
