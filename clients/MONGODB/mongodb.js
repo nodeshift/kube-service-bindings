1df03bf497c92e898e4cc6b99a312e0e618b290f
Costas Papastathis <papastathiscr@gmail.com>
2022-06-06 19:34:09 +0300

    refactor: mongodb transform function

--- clients/MONGODB/mongodb.js
+++ clients/MONGODB/mongodb.js
@@ -9,27 +9,19 @@ module.exports = {
     type: ''
   },
   transform: (binding) => {
-    let userPassword = '';
-    if (binding.connectionOptions &&
-        binding.connectionOptions.auth &&
-        binding.connectionOptions.auth.username) {
-      const encodedUser =
-        encodeURIComponent(binding.connectionOptions.auth.username);
-      let encodedPassword = '';
-      if (binding.connectionOptions.auth.password) {
-        encodedPassword =
-          encodeURIComponent(binding.connectionOptions.auth.password);
-      }
-      userPassword = `${encodedUser}:${encodedPassword}@`;
-    }
-    if (binding.srv === 'true') {
-      binding.url = `mongodb+srv://${userPassword}${binding.host}`;
-    } else {
-      let port = '';
-      if (binding.port) {
-        port = `:${binding.port}`;
-      }
-      binding.url = `mongodb://${userPassword}${binding.host}${port}`;
-    }
+    const encodedUser = encodeURIComponent(
+      binding.connectionOptions.auth.username
+    );
+
+    const encodedPassword = binding.connectionOptions.auth.password
+      ? encodeURIComponent(binding.connectionOptions.auth.password)
+      : '';
+
+    binding.url = [
+      'mongodb',
+      binding.srv === 'true' ? '+srv' : '',
+      `://${encodedUser}:${encodedPassword}@${binding.host}`,
+      binding.srv !== 'true' && binding.port ? `:${binding.port}` : ''
+    ].join('');
   }
 };
--- test/test-mongodb-bindings/test.js
+++ test/test-mongodb-bindings/test.js
@@ -3,27 +3,29 @@ const path = require('path');
 const { after, before, describe, it } = require('mocha');
 const bindings = require('../../index.js');
 
-describe('test-mongodb-bindings', () => {
+describe('MongoDB', () => {
   let env;
   before(() => {
     env = process.env;
     process.env = { SERVICE_BINDING_ROOT: path.join(__dirname, 'bindings') };
   });
 
-  it('test-mongodb', () => {
-    const binding = bindings.getBinding('MONGODB', 'mongodb');
-    assert(binding);
-    assert.deepEqual(binding, {
-      connectionOptions: {
-        auth: {
-          password: 'p1',
-          username: 'michael'
-        }
-      },
-      host: 'test.ourdomain.com',
-      port: '1234',
-      srv: 'true',
-      url: 'mongodb+srv://michael:p1@test.ourdomain.com'
+  describe('mongodb Client', () => {
+    it('Default behavior', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongodb');
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        host: 'test.ourdomain.com',
+        port: '1234',
+        srv: 'true',
+        url: 'mongodb+srv://michael:p1@test.ourdomain.com'
+      });
     });
   });
 
