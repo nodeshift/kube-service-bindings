a3570ce01537cc7a0678a802a0d02a5d4384c7c3
Costas Papastathis <papastathiscr@gmail.com>
2022-06-16 18:26:53 +0300

    test: adding more tests for mongoose and merging mongodb test cases

--- test/test-mongodb-bindings/test.js
+++ test/test-mongodb-bindings/test.js
@@ -10,9 +10,11 @@ describe('MongoDB', () => {
     process.env = { SERVICE_BINDING_ROOT: path.join(__dirname, 'bindings') };
   });
 
-  describe('mongodb Client', () => {
-    it('Default behavior', () => {
-      const binding = bindings.getBinding('MONGODB', 'mongodb');
+  describe('mongodb Client on mongodb-bindings', () => {
+    it('Default behaviour', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongodb', {
+        id: 'mongodb-bindings'
+      });
       assert(binding);
       assert.deepEqual(binding, {
         connectionOptions: {
@@ -29,9 +31,65 @@ describe('MongoDB', () => {
     });
   });
 
-  describe('mongoose Client', () => {
+  describe('mongodb Client on mongodb-bindings-with-port', () => {
+    it('Default behaviour id: mongodb-bindings-with-port', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongodb', {
+        id: 'mongodb-bindings-with-port'
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        host: 'test.ourdomain.com',
+        port: '1234',
+        url: 'mongodb://michael:p1@test.ourdomain.com:1234'
+      });
+    });
+  });
+
+  describe('mongoose Client on mongodb-bindings ', () => {
     it('Default behavior', () => {
-      const binding = bindings.getBinding('MONGODB', 'mongoose');
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        id: 'mongodb-bindings'
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        url: 'mongodb+srv://michael:p1@test.ourdomain.com'
+      });
+    });
+
+    it('Remove unmapped values', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        removeUnmapped: true,
+        id: 'mongodb-bindings'
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        url: 'mongodb+srv://michael:p1@test.ourdomain.com'
+      });
+    });
+
+    it('Do NOT Remove unmapped values', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        removeUnmapped: false,
+        id: 'mongodb-bindings'
+      });
       assert(binding);
       assert.deepEqual(binding, {
         connectionOptions: {
@@ -44,6 +102,60 @@ describe('MongoDB', () => {
         port: '1234',
         srv: 'true',
         url: 'mongodb+srv://michael:p1@test.ourdomain.com'
+      });
+    });
+  });
+
+  describe('mongoose Client on mongodb-bindings-with-port', () => {
+    it('Default Behaviour', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        id: 'mongodb-bindings-with-port'
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        url: 'mongodb://michael:p1@test.ourdomain.com:1234'
+      });
+    });
+
+    it('Remove unmapped values', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        id: 'mongodb-bindings-with-port',
+        removeUnmapped: true
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        url: 'mongodb://michael:p1@test.ourdomain.com:1234'
+      });
+    });
+
+    it('Remove unmapped values', () => {
+      const binding = bindings.getBinding('MONGODB', 'mongoose', {
+        id: 'mongodb-bindings-with-port',
+        removeUnmapped: false
+      });
+      assert(binding);
+      assert.deepEqual(binding, {
+        connectionOptions: {
+          auth: {
+            password: 'p1',
+            username: 'michael'
+          }
+        },
+        host: 'test.ourdomain.com',
+        port: '1234',
+        url: 'mongodb://michael:p1@test.ourdomain.com:1234'
       });
     });
   });
