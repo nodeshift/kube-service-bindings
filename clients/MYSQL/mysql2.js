6f6dd314f65eba025a90d8b27f67353707108ed9
Costas Papastathis <papastathiscr@gmail.com>
2022-07-04 18:13:15 +0300

    refactor: mysql mappings removing duplication (#50)

--- clients/MYSQL/mysql2.js
+++ clients/MYSQL/mysql2.js
@@ -1,14 +1,6 @@
-const { filterObject } = require('../../utils/index.js');
-
-const mapping = {
-  host: 'host',
-  port: 'port',
-  username: 'user',
-  database: 'database',
-  password: 'password'
-};
+const { mapping, filter } = require('./mysql.js');
 
 module.exports = {
   mapping,
-  filter: (binding) => filterObject(binding, Object.values(mapping))
+  filter
 };
